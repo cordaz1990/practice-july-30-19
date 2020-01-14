e65a677415a622e11bb94b22e6c5840feaadf60b
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 21:48:18 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,9 @@
-print('hello')
-print('world')
-print('hello')
+for cheese in cheeses:
+    print(cheese)
+
+for i in range(len(numbers)):
+    numbers[i] = numbers[i] * 2
+    
+for x in []:
+  print('This never happens.')
+  
